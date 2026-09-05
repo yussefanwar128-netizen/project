e418a14581e406b875f194b94b3244331c5c0640
Yussef <yussefanwar128@gmail.com>
2026-09-05 03:00:03 +0300

    Add GPA project

--- multip.py
+++
@@ -1,6 +0,0 @@
-print("\tThe First Project (Python)\n")
-print("\tMultiplication Table\n")
-num = float(input("Enter The num : "))
-for x in range(0,13):
-    print(num,"*",x,"=",num*x)
-print("\tThank You")
